9fda4db404ffc089e9ee4a5cd3b90fd319d3193f
Muhamad Yusuf Hidayat <muhamadyusufh50@gmail.com>
2023-11-20 00:01:47 +0700

    Pesan commit Anda di sini

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\Kuliah\\Semester 5\\Portfolio Website\\porto_web\\app\\page": [
+      "D:\\Kuliah\\Semester 5\\Portfolio Website\\porto_web\\styles\\globals.css"
+    ]
+  },
+  "D:\\Kuliah\\Semester 5\\Portfolio Website\\porto_web\\app\\layout.tsx": [
+    "D:\\Kuliah\\Semester 5\\Portfolio Website\\porto_web\\styles\\globals.css"
+  ]
+}
